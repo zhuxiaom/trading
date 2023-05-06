2eaa09d516508cd4a40ea2438c9dc28d65e1a357
Xiaomin Zhu <zhuxiaom@msn.com>
2023-05-06 05:51:38 -0700

    Revert "Rename uBlockOrigin"
    This reverts commit 20e1e674887f8dc3130d0daa46384967b37ad3a4.

--- zenzic/strategies/pytorch/scinet/price/sp500.cmd
+++
@@ -1,3 +0,0 @@
-set PYTHONPATH=C:\Users\xzhu\Documents\GitHub\trading\;C:\Users\xzhu\Documents\GitHub\trading\zenzic\thirdparty\SCINet\
-
-python -u sp500.py --start=2015-01-01 --end=2021-04-09 --out_file=C:\Trading\Prices\SP500\2023-02-20\train-data.pkl
